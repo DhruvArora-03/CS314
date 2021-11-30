3b2f17df8994bbaa3d3f49a2adb40a460beaa588
Dhruv Arora <dhruvarora.us@gmail.com>
2021-11-29 20:44:55 -0600

    Moved PQ to its own file

--- Assignment_10 - Pair Assignment/SimpleHuffProcessor.java
+++ Assignment_10 - Pair Assignment/SimpleHuffProcessor.java
@@ -20,7 +20,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Arrays;
-import java.util.NoSuchElementException;
 
 public class SimpleHuffProcessor implements IHuffProcessor {
 
@@ -378,83 +377,6 @@ public class SimpleHuffProcessor implements IHuffProcessor {
         @Override
         public String toString() {
             return "HuffCode [numBits=" + numBits + ", value=" + value + "]";
-        }
-    }
-
-    private class PQ<E extends Comparable<E>> {
-        private final Node<E> HEADER; // data is NOT sorted
-        private Node<E> last;
-
-        private PQ() {
-            HEADER = new Node<E>(null, null);
-            last = HEADER;
-        }
-
-        /**
-         * Adds a given element to the priority queue.
-         * 
-         * @param element E to be added, element != null
-         */
-        private void enqueue(E element) {
-            if (element == null) {
-                throw new IllegalArgumentException("element cannot be null");
-            }
-
-            last.next = new Node<E>(element, null);
-            last = last.next;
-        }
-
-        /**
-         * Removes and returns the element with the lowest priority. Cannot be used on empty queue.
-         * 
-         * @return The element with the lowest priority.
-         */
-        private E dequeue() {
-            if (last == HEADER) {
-                throw new NoSuchElementException("Priority queue is empty");
-            }
-
-            // search for the min element, not updating if tied
-            Node<E> min = HEADER.next;
-            Node<E> prevMin = HEADER;
-            Node<E> curr = HEADER.next;
-            Node<E> prevCurr = HEADER;
-
-            while (curr != null) {
-                E currVal = curr.value;
-
-                if (min.value.compareTo(currVal) > 0) { // min > curr --> min = curr
-                    min = curr;
-                    prevMin = prevCurr;
-                }
-
-                prevCurr = curr;
-                curr = curr.next;
-            }
-            prevMin.next = min.next; // remove
-
-            if (min == last) {
-                last = prevMin;
-            }
-
-            return min.value;
-        }
-
-        /**
-         * Tells us if the queue has a size of one.
-         */
-        private boolean isSizeOne() {
-            return last == HEADER.next;
-        }
-
-        private class Node<E extends Comparable<E>> {
-            E value;
-            Node<E> next;
-
-            Node(E value, Node<E> next) {
-                this.value = value;
-                this.next = next;
-            }
         }
     }
 }
